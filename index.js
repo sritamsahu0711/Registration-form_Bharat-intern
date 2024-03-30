5087305d5f69ed4f37b96e8f267ca6c50941a226
sritamsahu0711 <163115674+sritamsahu0711@users.noreply.github.com>
2024-03-30 10:02:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+var express=require("express")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/Database')
+var db=mongoose.connection
+db.on('error',()=> console.log("Error while Connecting to Database"))
+db.once('open',()=> console.log("Connected to Database successfully"))
+
+app.post("/sign_up",(req,res) => {
+    var firstname= req.body.firstname
+    var lastname=req.body.lastname
+    var email=req.body.email
+    var password=req.body.password
+
+    var data={
+        "firstname":firstname,
+        "lastname":lastname,
+        "email":email,
+        "password":password
+    }
+    db.collection('users').insertOne(data,(err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Succesfully")
+    })
+    return res.redirect('signup_successful.html')
+})
+
+app.get("/",(req,res) => {
+    res.set({
+        "Allow-acces-Allow-Origin":'*'
+    })
+    return res.redirect('index.html')
+}).listen(3000);
+
+console.log("Listening on port 3000")
